58bc602b2f4438136912ef6f9b423215a58c377d
DevTech <contadigitalsd@gmail.com>
2026-01-20 11:17:07 -0600

    🔧 Cambiada ruta principal a landing page

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
-
+**/
+Route::get('/', function () {
+    return view('landing');
+});
 
 Route::get('/landinpage', function () {
     return view('landinpage');
